1c14c67b666b9ebfad557128b7897744cf8b29cd
benlaflen <114254189+benlaflen@users.noreply.github.com>
2025-06-22 22:51:11 -0700

    Updates

--- qpl_deflate.c
+++ qpl_deflate.c
@@ -20,9 +20,9 @@ int qpl_deflate_run(qpl_deflate_stream *stream,
                     const void *src, size_t src_len,
                     void *dst, size_t dst_capacity,
                     size_t *compressed_size) {
-    printf("src_len = %u\n", src_len);
-    printf("dst_capacity = %u\n", dst_capacity);
-    printf("compressed_size = %u\n", compressed_size);
+    printf("src_len = %lu\n", src_len);
+    printf("dst_capacity = %lu\n", dst_capacity);
+    printf("compressed_size = %lu\n", compressed_size);
     
     qpl_job *job = stream->job;
 
