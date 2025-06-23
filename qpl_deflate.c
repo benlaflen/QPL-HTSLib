55a7314c3b94ab29dd9029b11f56a177adb5167f
benlaflen <114254189+benlaflen@users.noreply.github.com>
2025-06-22 22:30:18 -0700

    Updates

--- qpl_deflate.c
+++ qpl_deflate.c
@@ -1,5 +1,6 @@
 #include "qpl_deflate.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 int qpl_deflate_init(qpl_deflate_stream *stream) {
     qpl_status status = qpl_get_job_size(qpl_path_software, &stream->job_size);
